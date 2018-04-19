9f09d7386e32659766414d0c1645d8bc89a61e75
haohaoxing <1005291507@qq.com>
2018-04-19 15:32:37 +0800

    4.19haoxing jiaoshoujia

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<!--  -->
+<template>
+  <div>
+      <my-header></my-header>
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+    import MyHeader from "../components/MyHeader.vue";
+export default {
+  data () {
+    return {
+    };
+  },
+//注册一下
+  components: {
+      MyHeader
+  },
+
+  computed: {},
+
+  mounted(){},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
